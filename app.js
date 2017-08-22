774468223a48a8eaaff01ba3adebd1abab260f9f
stone <xiaochonzi@outlook.com>
2017-08-23 07:39:37 +0800

    add wx-ui

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ App({
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
+    console.log('onLaunch')
     wx.setStorageSync('logs', logs)
   },
 
   getUserInfo: function(cb) {
     var that = this
+    console.log('start getUserInfo')
     if (this.globalData.userInfo) {
       typeof cb == "function" && cb(this.globalData.userInfo)
     } else {
@@ -16,10 +18,12 @@ App({
       wx.getUserInfo({
         withCredentials: false,
         success: function(res) {
+          console.log(res)
           that.globalData.userInfo = res.userInfo
           typeof cb == "function" && cb(that.globalData.userInfo)
         }
       })
+      console.log('finish getUserInfo')
     }
   },
 
